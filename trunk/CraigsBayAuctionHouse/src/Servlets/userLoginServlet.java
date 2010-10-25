d41a3d52d443c3e35d2e2043319b6ff590c61da1
martinku86 <martinku86@1c5a4b77-2332-d09a-46a7-932d11b47734>
2010-10-25 11:16:45 +0000

    - Added Servlets for login and checking login - Added DBmanager function for login and checking login - Attempted to modify Login page to send correct request Martin

---
+++ trunk/CraigsBayAuctionHouse/src/Servlets/userLoginServlet.java
@@ -0,0 +1,54 @@
+package Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Managers.DBManager;
+
+public class userLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public userLoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userName = request.getParameter("userName").toString();
+		String password = request.getParameter("password").toString();
+		
+		DBManager dbm = new DBManager();
+		dbm.userLogin(userName, password);
+		
+		// Write XML to response if DB has return message
+//		StringBuffer XMLResponse = new StringBuffer();	
+//		XMLResponse.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+//		XMLResponse.append("<response>\n");
+//		XMLResponse.append("\t<toAddress>" + toAddress + "</toAddress>\n");
+//		XMLResponse.append("\t<subject>" + subject + "</subject>\n");
+//		XMLResponse.append("\t<body>" + body + "</body>\n");
+//		XMLResponse.append("</response>\n");
+//			
+//		response.setContentType("application/xml");
+//		response.getWriter().println(XMLResponse);
+	}
+
+}

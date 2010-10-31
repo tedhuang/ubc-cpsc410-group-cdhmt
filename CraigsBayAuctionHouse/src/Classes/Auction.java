03469742db56fe9ab8cbec0919056c586a872551
chrislack42 <chrislack42@1c5a4b77-2332-d09a-46a7-932d11b47734>
2010-10-31 22:55:36 +0000

    ** Moved Auction class to correct place

---
+++ CraigsBayAuctionHouse/src/Classes/Auction.java
@@ -0,0 +1,24 @@
+
+public class Auction {
+	
+	public int auctionID;
+	public String auctionTitle;
+	public double expiryDate;
+	public double creationDate;
+	public String category;
+	public int ownerID;
+	public int lastBidder;
+	public double minPrice;
+	public double lastestBidPrice;
+	public int bidCounter;
+	public String auctionStatus;
+	public String flickerAlbumID;
+	public int numberOfViews;
+	
+	public void toXML(){
+		
+	}
+	
+	
+
+}

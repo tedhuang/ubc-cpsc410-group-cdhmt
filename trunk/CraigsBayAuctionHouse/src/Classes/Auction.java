9a81d15da5cf6b407e452339876b740effbd6494
chrislack42 <chrislack42@1c5a4b77-2332-d09a-46a7-932d11b47734>
2010-10-31 23:45:50 +0000

    ** Added toXML method to Auction -- Chris

--- trunk/CraigsBayAuctionHouse/src/Classes/Auction.java
+++ trunk/CraigsBayAuctionHouse/src/Classes/Auction.java
@@ -1,5 +1,11 @@
 package Classes;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class Auction {
 	
 	public int auctionID;
@@ -16,8 +22,31 @@ public class Auction {
 	public String flickerAlbumID;
 	public int numberOfViews;
 	
-	public void toXML(){
+	public void toXML(HttpServletResponse response) throws ServletException, IOException{
 		
+		//Write XML
+		StringBuffer XMLResponse = new StringBuffer();	
+		XMLResponse.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+		XMLResponse.append("<response>\n");
+		
+		XMLResponse.append("\t<auctionID>" + auctionID + "</auctionID>\n + " +
+							"\t<auctionTitle>" + auctionTitle + "</auctionTitle>\n" +
+							"\t<expiryDate>" + expiryDate + "</expiryDate>\n" +
+							"\t<creationDate>" + creationDate + "</creationDate>\n"
+							"\t<category>" + category + "</category>\n" +
+							"\t<ownerID>" + ownerID + "</ownerID>\n" +
+							"\t<lastBidder>" + lastBidder + "</lastBidder>\n" +
+							"\t<minPrice>" + minPrice + "</minPrice>\n" +
+							"\t<lastestBidPrice>" + lastestBidPrice + "</lastestBidPrice>\n" +
+							"\t<bidCounter>" + bidCounter + "</bidCounter>\n" +
+							"\t<auctionStatus>" + auctionStatus + "</auctionStatus>\n" +
+							"\t<flickerAlbumID>" + flickerAlbumID + "</flickerAlbumID>\n" +
+							"\t<numberOfViews>" + numberOfViews + "</numberOfViews>\n");
+		XMLResponse.append("</response>\n");
+		
+		response.setContentType("application/xml");
+		response.getWriter().println(XMLResponse);
+				
 	}
 	
 	

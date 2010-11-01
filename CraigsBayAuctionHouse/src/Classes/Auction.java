f3622f367617b1ce7f4cdbd0e5e36e6599e0a97f
chrislack42 <chrislack42@1c5a4b77-2332-d09a-46a7-932d11b47734>
2010-11-01 03:06:00 +0000

    * Changed XML format in Auction.java * Added extra XML tags in listAuctionsServlet - Chris

--- CraigsBayAuctionHouse/src/Classes/Auction.java
+++ CraigsBayAuctionHouse/src/Classes/Auction.java
@@ -26,19 +26,21 @@ public class Auction {
 	//TODO: make sure null entry and null value could be processed for a certain field
 	public String toXMLContent() {
 		
-		return "\t<auctionID>" + auctionID + "</auctionID>\n" +
-				"\t<auctionTitle>" + auctionTitle + "</auctionTitle>\n" +
-				"\t<expiryDate>" + expiryDate + "</expiryDate>\n" +
-				"\t<creationDate>" + creationDate + "</creationDate>\n" +
-				"\t<category>" + category + "</category>\n" +
-				"\t<ownerID>" + ownerID + "</ownerID>\n" +
-				"\t<lastBidderID>" + lastBidderID + "</lastBidderID>\n" +
-				"\t<minPrice>" + minPrice + "</minPrice>\n" +
-				"\t<latestBidPrice>" + latestBidPrice + "</latestBidPrice>\n" +
-				"\t<bidCounter>" + bidCounter + "</bidCounter>\n" +
-				"\t<auctionStatus>" + auctionStatus + "</auctionStatus>\n" +
-				"\t<flickerAlbumID>" + flickerAlbumID + "</flickerAlbumID>\n" +
-				"\t<numberOfViews>" + numberOfViews + "</numberOfViews>\n";
+		return "\t<auction" +
+				" auctionID =" + auctionID +
+				" auctionTitle =" + auctionTitle +
+				" expiryDate =" + expiryDate  +
+				" creationDate =" + creationDate +
+				" category =" + category +
+				" ownerID =" + ownerID +
+				" lastBidderID =" + lastBidderID +
+				" minPrice =" + minPrice +
+				" latestBidPrice =" + latestBidPrice +
+				" bidCounter =" + bidCounter +
+				" auctionStatus =" + auctionStatus +
+				" flickerAlbumID =" + flickerAlbumID +
+				" numberOfViews =" + numberOfViews + 
+				"/>\n";
 				
 	}
 	

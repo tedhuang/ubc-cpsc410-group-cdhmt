cacc131316a20f040daa8e4d795abf9abc462255
Tedhuang88@gmail.com <Tedhuang88@gmail.com@1c5a4b77-2332-d09a-46a7-932d11b47734>
2010-11-08 02:11:09 +0000

    Partial Flickr Implementation

---
+++ trunk/CraigsBayAuctionHouse/src/Managers/FlickrManager.java
@@ -0,0 +1,29 @@
+package Managers;
+
+//import java.sql.*;
+import java.util.*;
+
+//import DBConnection.DBConn;
+
+public class FlickrManager {
+	
+	public FlickrManager(){}
+	
+	//TODO: for later, link photoID with user of the site
+	public String getPhotoID(String apiKey, String userID){
+		
+		String photoID = null;
+		
+		//
+		
+		
+		return photoID;
+	}
+	
+	
+	//TODO: implement upload function
+	
+	
+	
+
+}

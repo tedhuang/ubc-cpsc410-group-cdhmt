3ad19f4a758feb2c23d9a66469aed59a6d050c1d
martinku86 <martinku86@1c5a4b77-2332-d09a-46a7-932d11b47734>
2010-11-25 00:47:22 +0000

    *  Basic styled p2p chat servlet done * chatting test page setup - Martin

--- trunk/CraigsBayAuctionHouse/src/Servlets/chatRelayServlet.java
+++ trunk/CraigsBayAuctionHouse/src/Servlets/chatRelayServlet.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.sql.*;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import Managers.DBManager;
 
@@ -15,14 +20,21 @@ import Managers.DBManager;
  */
 public class chatRelayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 	
-	int aggregatingCounter = 0;
+//	Map<Integer, HttpServletResponse> chatOngoingMap;
+//	Map<Integer, HttpServletResponse> chatIdleMap;
+	Map<Integer, StringBuffer > sessionMsgMap;
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
     public chatRelayServlet() {
         super();
+        
+        
+//        chatOngoingMap = new ConcurrentHashMap<Integer, HttpServletResponse>();
+//        chatIdleMap = new ConcurrentHashMap<Integer, HttpServletResponse>();
+        sessionMsgMap = new ConcurrentHashMap<Integer, StringBuffer >();
         
         DBManager dbm = new DBManager();
         
@@ -30,41 +42,97 @@ public class chatRelayServlet extends HttpServlet {
         
         //TODO use statment without disconnecting
     }
+	
+	private int cantorPairing( int key1, int key2 ) {
+		
+		return ( ((key1 + key2) * (key1 + key2 + 1) ) / 2  ) + key2;
+		
+	}
     
-    public void init()
-    {
-    	//System.out.println("IM ALIVE");
-    }
-
+	public int registerChatSession(int senderID, int receiverID ) {
+		
+		int pollingCode = cantorPairing( receiverID, senderID) ;
+		
+		sessionMsgMap.put(pollingCode, new StringBuffer() );
+		
+		return pollingCode;
+	}
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		// Registering the user's response for chat
+		Integer userID = Integer.parseInt( request.getParameter("userID") );
+		
+		StringBuffer tempBuffer;
+		if( sessionMsgMap.containsKey( userID ) ) {
+			tempBuffer = sessionMsgMap.remove( userID );
+		}
+		else {
+			tempBuffer = new StringBuffer("\t<message></message>");
+		}
+		
+		StringBuffer XMLResponse = new StringBuffer();	
+		XMLResponse.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+		XMLResponse.append("<response>\n");
+		
+		XMLResponse.append( tempBuffer );
+		
+		XMLResponse.append("</response>\n");
+		response.setContentType("application/xml");
+		response.getWriter().println(XMLResponse);
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// get ID for sender and receiver
+		String senderIDString = request.getParameter("senderID").toString();
+		String sendToIDString = request.getParameter("sendToID").toString();
 		
-		String addThis = request.getParameter("addThis").toString();
+		Integer senderID = Integer.valueOf(senderIDString);
+		Integer sendToID = Integer.valueOf(sendToIDString);
 		
-		aggregatingCounter += Integer.parseInt(addThis);
- 
-		System.out.println(aggregatingCounter);
+		
+		// generate the key pairing
+//		int pairKeyTo = cantorPairing( senderID.intValue(), sendToID.intValue() );
+//		int pairKeyFrom = cantorPairing( sendToID.intValue(), senderID.intValue()  );
+		
+		String msg = request.getParameter("message").toString();
+		System.out.println("sending message from " + senderID + " to " + sendToID + " msg: " + msg );
+		
+		StringBuffer tempBuffer;
+		
+		if ( sessionMsgMap.containsKey( sendToID ) ) {
+			tempBuffer = sessionMsgMap.get( sendToID );
+		}
+		else {
+			tempBuffer = new StringBuffer();
+		}
+
+		
+		tempBuffer.append("\t<message" +
+							" senderID=\"" + senderIDString + "\"" +
+							" timeStamp=\"" + Calendar.getInstance().getTimeInMillis() + "\"" +
+							">" + msg + "</message>\n");
+								
+		sessionMsgMap.put( sendToID, tempBuffer );
 		// Write XML to response if DB has return message
-		StringBuffer XMLResponse = new StringBuffer();	
-		XMLResponse.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
-		XMLResponse.append("<response>\n");
+//		StringBuffer XMLResponse = new StringBuffer();	
+//		XMLResponse.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+//		XMLResponse.append("<response>\n");
+//		
+//		XMLResponse.append("\t<message>" + msg + "</message>\n");
+//		
+//		XMLResponse.append("</response>\n");
+//		writeToResponse.setContentType("application/xml");
+//		writeToResponse.getWriter().println(XMLResponse);
 		
-		XMLResponse.append("\t<result>" + aggregatingCounter + "</result>\n");
-		
-		XMLResponse.append("</response>\n");
-		response.setContentType("application/xml");
-		response.getWriter().println(XMLResponse);
-		
+
 	}
 
 }

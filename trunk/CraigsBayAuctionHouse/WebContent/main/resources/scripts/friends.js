16bd506b23bbdfe79f5d20cfb0be49194bb4cdb2
abigrabit <abigrabit@1c5a4b77-2332-d09a-46a7-932d11b47734>
2010-11-25 02:10:13 +0000

    harry, friends fixed

--- trunk/CraigsBayAuctionHouse/WebContent/main/resources/scripts/friends.js
+++ trunk/CraigsBayAuctionHouse/WebContent/main/resources/scripts/friends.js
@@ -1,38 +1,51 @@
-function loadFriends()
+function viewFriends(container)
+{
+		var credential = document.getElementById("cred").value;
+		
+		if(credential!=null)
+		   {
+			showFriendTab();
+			ajaxpage('friendsPage.jsp', container); //load tableLoad.jsp in to div Dynapage
+			document.getElementById("friendTbTitle").innerHTML="<img src=./resources/images/loading.gif></img>";
+		   }
+		
+}
+
+function loadFriends(container)
 {	
 	var credential = document.getElementById("cred").value;
 	
+		if (window.XMLHttpRequest)
+		  {// code for IE7+, Firefox, Chrome, Opera, Safari
+		  xmlhttp=new XMLHttpRequest();
+		  }
+		else
+		  {// code for IE6, IE5
+		  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		  }
+		  
+		xmlhttp.onreadystatechange=function()
+		{
+		  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		    {
+			    //parse XML response from server
+			    
+			    var responseText= ParseFriendsList(xmlhttp.responseXML, container);
+		    	
+		    }
+		  }
 	
-	if (window.XMLHttpRequest)
-	  {// code for IE7+, Firefox, Chrome, Opera, Safari
-	  xmlhttp=new XMLHttpRequest();
-	  }
-	else
-	  {// code for IE6, IE5
-	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	  }
+		//send the parameters to the servlet with POST
+		var Params = "Credential=" + credential;
+		
+		xmlhttp.open("POST","../friendsListServlet" ,true);
+		xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xmlhttp.send(Params);
 	  
-	xmlhttp.onreadystatechange=function()
-	{
-	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	    {
-		    //parse XML response from server
-		    
-		    var responseText= ParseFriendsList(xmlhttp.responseXML);
-	    	
-	    }
-	  }
-
-	//send the parameters to the servlet with POST
-	var Params = "Credential=" + credential;
-	
-	xmlhttp.open("POST","../friendsListServlet" ,true);
-	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xmlhttp.send(Params);
-	//document.getElementById("myDiv").innerHTML="<h2>Please wait...getting entry</h2>";
+		//document.getElementById("myDiv").innerHTML="<h2>Please wait...getting entry</h2>";
 }
 
-function ParseFriendsList(responseXML){
+function ParseFriendsList(responseXML, container){
 	 var success = (responseXML.getElementsByTagName("success")[0]).childNodes[0].nodeValue;
 	 
 	if(success=="false")
@@ -44,7 +57,7 @@ function ParseFriendsList(responseXML){
 	var friendList = responseXML.getElementsByTagName('friendList').item(0);
 	
 	//remove old elements
-	var oBody = document.getElementById('myTable');
+	var oBody = document.getElementById(container);
 	while(oBody.hasChildNodes())
 	{
 		oBody.removeChild(oBody.firstChild);
@@ -70,56 +83,56 @@ function ParseFriendsList(responseXML){
 		rowParams[2] = "<a class=\"button\" onclick=deleteFriend("+friendID+");>Delete</a>";
 		rowParams[3] = "<a class=\"button\" onclick=searchAuctionTable(2,\""+friendName+"\");>View Auctions</a>";
 		rowParams[4] = "<a class=\"button\" onclick=chatFriend("+friendID+");>Chat</a>";
-		addElement2(rowParams);
+		addElement(rowParams, container);
 	}
-	document.getElementById("surferTitle").innerHTML="Friend List";
+	document.getElementById("friendTbTitle").innerHTML="Friend List";
 }
 
-function addElement2( rowParams ) {
-	
-	  var ni = document.getElementById('myTable');
-	  
-	  var numi = document.getElementById('theValue');
-	  var num = (document.getElementById('theValue').value -1)+ 2;
-	  numi.value = num;
-	  var newdiv = document.createElement('div');
-	  var divIdName = 'my'+num+'Div';
-	  newdiv.setAttribute('id',divIdName);
-	  newdiv.setAttribute('class','div-table-row');
-	 
-	  //remove old rows..
-	  
-	  var oBody = document.getElementById('myTable');
-	  
-	 // Insert rows and cells into bodies.
-	    oRow = oBody.insertRow(oBody.rows.length);
-	    oRow.setAttribute('id', rowParams[0]);
-
-	   //Friend ID
-	    oCell = oRow.insertCell(-1);
-	    
-	    oCell.innerHTML =  rowParams[0];
-	    
-	    //Friend Name
-	    oCell = oRow.insertCell(-1);
-	    oCell.innerHTML =  rowParams[1];
-	    
-	    //Delete Button
-	    oCell = oRow.insertCell(-1);
-	    oCell.innerHTML =  rowParams[2];
-	    
-	    //Auction Button
-	    oCell = oRow.insertCell(-1);
-	    oCell.innerHTML =  rowParams[3];
-	    
-	    //Chat Button
-	    oCell = oRow.insertCell(-1);
-	    oCell.innerHTML =  rowParams[4];
-	    
-	  	//tools
-	    
-	  //ni.appendChild(newdiv);
-	}
+//function addElement2( rowParams, container ) {
+//	
+//	  var ni = document.getElementById('myTable');
+//	  
+//	  var numi = document.getElementById('theValue');
+//	  var num = (document.getElementById('theValue').value -1)+ 2;
+//	  numi.value = num;
+//	  var newdiv = document.createElement('div');
+//	  var divIdName = 'my'+num+'Div';
+//	  newdiv.setAttribute('id',divIdName);
+//	  newdiv.setAttribute('class','div-table-row');
+//	 
+//	  //remove old rows..
+//	  
+//	  var oBody = document.getElementById('myTable');
+//	  
+//	 // Insert rows and cells into bodies.
+//	    oRow = oBody.insertRow(oBody.rows.length);
+//	    oRow.setAttribute('id', rowParams[0]);
+//
+//	   //Friend ID
+//	    oCell = oRow.insertCell(-1);
+//	    
+//	    oCell.innerHTML =  rowParams[0];
+//	    
+//	    //Friend Name
+//	    oCell = oRow.insertCell(-1);
+//	    oCell.innerHTML =  rowParams[1];
+//	    
+//	    //Delete Button
+//	    oCell = oRow.insertCell(-1);
+//	    oCell.innerHTML =  rowParams[2];
+//	    
+//	    //Auction Button
+//	    oCell = oRow.insertCell(-1);
+//	    oCell.innerHTML =  rowParams[3];
+//	    
+//	    //Chat Button
+//	    oCell = oRow.insertCell(-1);
+//	    oCell.innerHTML =  rowParams[4];
+//	    
+//	  	//tools
+//	    
+//	  //ni.appendChild(newdiv);
+//	}
 
 function deleteFriend(friendID){
 	
@@ -141,7 +154,7 @@ var credential = document.getElementById("cred").value;
 	    {
 		    //parse XML response from server
 		    
-		    var responseText= ParseFriendDelete(xmlhttp.responseXML);
+		    var responseText= ParseFriendDelete(xmlhttp.responseXML, container);
 		    loadFriends();
 	    	
 	    }
@@ -156,7 +169,7 @@ var credential = document.getElementById("cred").value;
 	//document.getElementById("myDiv").innerHTML="<h2>Please wait...getting entry</h2>";
 }
 
-function ParseFriendDelete(responseXML){
+function ParseFriendDelete(responseXML, container){
 	
 	 var success = (responseXML.getElementsByTagName("success")[0]).childNodes[0].nodeValue;
 	 var responseText = "";
@@ -172,6 +185,6 @@ function ParseFriendDelete(responseXML){
 		 alert("Friend deleted!");
 	}
 	 //TODO Disable page while it reloads; ATM it reloads but the user can interact with it while it does.
-	 loadFriends();
+	 loadFriends(container);
 	 return responseText;
 }
